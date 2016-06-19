94cf970c54ab600ae1f1e4ef00cb9d79789dc1f6
David Alsh <alshdavi@gmail.com>
2016-06-19 01:14:42 -0400

    added startup script

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -------------------------------------------------
+# This script file is used as a start up script for
+# each software deployed on this droplet at boot up
+# -------------------------------------------------
+#
+# Rules
+#
+# 1) create a start up bash script in the directory of your software
+# 2) run that bash file from here
+# 3) enjoy
+#
+
+echo "Starting authHero -- api.authhero.com"
+screen -A -d -m -S authHero node authHero-API/app.js &
