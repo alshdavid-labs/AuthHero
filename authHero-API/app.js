e1138d8e37ba9b5049f036a548155dac72fb597c
David Alsh <alshdavid@gmail.com>
2016-06-25 01:20:58 +1000

    IP

--- authHero-API/app.js
+++ authHero-API/app.js
@@ -27,7 +27,7 @@ var URL = process.argv[2]
 //SELECT * FROM useraccounts;
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
