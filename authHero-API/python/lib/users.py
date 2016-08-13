522a1c965a9e5f9e1465cf37dca61ae956984eab
David Alsh <David Alsh>
2016-08-13 20:47:40 +1200

    started making the python service do what I want it to

--- authHero-API/python/lib/users.py
+++ authHero-API/python/lib/users.py
@@ -1,51 +1,6 @@
 import lib.db
 database = lib.db
 
-# Register User
-def userRegistration(data):
-    # Test inputs
-    if checkInput("userInformation", data) == False:
-        message = { "message" : "Invalid JSON" }
-        return message
-    # Assin tidy variables
-    username = data[0]['username']
-    password = data[0]['password']
-    # Check to see if account exsists
-    if checkUser(username) != 0:
-        message = { "message" : "User already exsists" }
-        return message
-    # Create the account
-    dbResult = registerUser(username, password)    
-    # Generate a sucess message
-    message = { "message" : "User account created"}
-    message.update(dbResult)
-    return message
-
-# Login User
-def userLogin(data):    
-    # Test inputs
-    if checkInput("userInformation", data) == False:
-        message = { "message" : "Invalid JSON" }
-        return message
-    # Assign tidy variables
-    username = data[0]['username']
-    password = data[0]['password']
-    # Check to see if account exsists
-    if checkUser(username) == 0:
-        message = { "message" : "User does not exsist" }
-        return message
-    # Compare the account details
-    if correctPassword(username, password) == False:
-        message = { "message" : "incorrect password" }
-        return message    
-    # Generate auth token
-    authResponse = generateAuth(username)
-    userObject = database.getUser(username)
-    # Generate a sucess message
-    message = { "message" : "User account logged in"}
-    message.update(authResponse)
-    message.update(userObject)
-    return message
 
 # Check auth token
 def checkAuth(data):
@@ -58,6 +13,60 @@ def checkAuth(data):
         return message 
     message = { "message" : "Valid auth token" }
     return message  
+
+# Register User
+def adminRegistration(data):
+    print('got this far')
+    print(data)
+    # Test inputs
+    if checkInput("userInformation", data) == False:
+        message = { "message" : "Invalid JSON" }
+        return message
+    # Assin tidy variables
+    username = data[0]['username']
+    password = data[0]['password']
+    # Check to see if account exsists
+    if checkAdmin(username) != 0:
+        message = { "message" : "Admin already exsists" }
+        return message
+    # Create the account
+    dbResult = registerAdmin(username, password)    
+    # Generate a sucess message
+    message = { "message" : "Admin account created"}
+    message.update(dbResult)
+    return message
+
+# Login User
+def adminLogin(data):    
+    # Test inputs
+    if checkInput("userInformation", data) == False:
+        message = { "message" : "Invalid JSON" }
+        return message
+    # Assign tidy variables
+    username = data[0]['username']
+    password = data[0]['password']
+    # Check to see if account exsists
+    if checkAdmin(username) == 0:
+        message = { "message" : "User does not exsist" }
+        return message
+    # Compare the account details
+    if checkAdminPassword(username, password) == False:
+        message = { "message" : "incorrect password" }
+        return message    
+    # Generate auth token
+    authResponse = loginAdmin(username)
+    #userObject = database.getUser(username)
+    # Generate a sucess message
+    message = { "message" : "User account logged in"}
+    message.update(authResponse)
+    #message.update(userObject)
+    return message
+
+
+
+
+
+
 
 # Check inputs
 def checkInput(toCheck, data):
@@ -75,17 +84,17 @@ def checkInput(toCheck, data):
             return False 
         return True  
 
-def checkUser(username):    
-    return database.checkUser(username)
+def checkAdmin(username):    
+    return database.checkAdmin(username)
 
-def correctPassword(username, password):    
-    return database.correctPassword(username, password)    
+def checkAdminPassword(username, password):    
+    return database.checkAdminPassword(username, password)    
 
-def registerUser(username, password):    
-    return database.registerUser(username, password)
+def registerAdmin(username, password):    
+    return database.registerAdmin(username, password)
 
-def generateAuth(username):    
-    return database.generateAuth(username)
+def loginAdmin(username):    
+    return database.loginAdmin(username)
 
 def validAuth(auth):    
     return database.checkAuth(auth)    
--- authHero-API/python/main.py
+++ authHero-API/python/main.py
@@ -1,3 +1,6 @@
+#$ pip install Flask
+#$ pip install -U flask-cors
+
 import threading
 
 import lib.users
@@ -11,26 +14,47 @@ app = Flask(__name__, static_url_path='')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
+#api redirect
 @app.route("/")
 def root():
-    return 'lol'  
+    return "<body style='margin:50px; text-align: center'><p>The authHero API</p><br><p>Go to authHero.com for usage instructions</p></body>"
 
-
-@app.route("/register", methods=['POST'])
-@cross_origin()
-def register():
-    return jsonify(users.userRegistration(request.get_json()))
-
-@app.route("/login", methods=['POST'])
-@cross_origin()
-def login():
-    return jsonify(users.userLogin(request.get_json()))        
-
+#test if auth token is registered
 @app.route("/auth", methods=['POST'])
 @cross_origin()
 def auth():
     return jsonify(users.checkAuth(request.get_json())) 
+
+#register admin account
+@app.route("/a/register", methods=['POST'])
+@cross_origin()
+def register():
+    return jsonify(users.adminRegistration(request.get_json()))
+
+#login admin account
+@app.route("/a/login", methods=['POST'])
+@cross_origin()
+def login():
+    return jsonify(users.adminLogin(request.get_json()))        
+
+#Modify project POST GET DELETE PUT
+@app.route("/a/project", methods=['POST'])
+@cross_origin()
+def project():
+    return 'soon'  
+
+#register user for a project
+@app.route("/u/<projectName>/register", methods=['POST'])
+@cross_origin()
+def projectRegister(projectName):
+    return projectName 
+
+#login user to a project
+@app.route("/u/<projectName>/login", methods=['POST'])
+@cross_origin()
+def projectLogin(projectName):
+    return projectName 
+
 
 @app.route("/menu/<username>", methods=['GET', 'PUT'])
 @cross_origin()
